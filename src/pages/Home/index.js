dd36fed2de9fe57eed052d5454bfb5972ef8395e
Claudia Heindrickson <claudia.heindrickson@gmail.com>
2020-08-01 20:43:04 -0300

    .

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,19 @@
-import React from "react";
-import Menu from "../../components/Menu";
-import BannerMain from "../../components/BannerMain";
-import Carousel from "../../components/Carousel";
-import Footer from "../../components/Footer";
-import dadosIniciais from "../../data/dados_iniciais.json";
+import React from 'react';
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
+import dadosIniciais from '../../data/dados_iniciais.json';
 
 function Home() {
   return (
-    <div style={{ backgroundColor: "#141414" }}>
+    <div style={{ backgroundColor: '#141414' }}>
       <Menu />
 
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={
-          "Jocko Willink, autor do best-seller: Extreme Ownership: How U.S. Navy SEALs Lead and Win (sem versão traduzida para o português), oferece alguns conselhos práticos, porém difíceis de aprender. Tudo começa com a disciplina. Esse é o caminho para a realização pessoal e para o sucesso. "
-        }
+        videoDescription="Jocko Willink, autor do best-seller: Extreme Ownership: How U.S. Navy SEALs Lead and Win (sem versão traduzida para o português), oferece alguns conselhos práticos, porém difíceis de aprender. Tudo começa com a disciplina. Esse é o caminho para a realização pessoal e para o sucesso. "
       />
 
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
